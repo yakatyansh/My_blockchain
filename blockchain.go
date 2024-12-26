a33b5cd6b3be1b6edb0503d507f995739e45d878
yakatyansh <ykpetrolhead08@outlook.com>
2024-12-26 13:28:13 +0530

    genesis block

--- blockchain.go
+++ blockchain.go
@@ -1,4 +1,11 @@
-package golangblockchain
+package main
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"time"
+)
 
 type Block struct {
 	Index     int
@@ -6,4 +13,56 @@ type Block struct {
 	Data      string
 	PrevHash  string
 	Hash      string
+}
+
+var Blockchain []Block
+
+func calculateHash(block Block) string {
+	record := fmt.Sprintf("%d%s%s%s", block.Index, block.Timestamp, block.Data, block.PrevHash)
+	h := sha256.New()
+	h.Write([]byte(record))
+	return hex.EncodeToString(h.Sum(nil))
+}
+
+func generateBlock(oldBlock Block, Data string) (Block, error) {
+	var newBlock Block
+
+	t := time.Now()
+
+	newBlock.Index = oldBlock.Index + 1
+	newBlock.Timestamp = t.String()
+	newBlock.Data = Data
+	newBlock.PrevHash = oldBlock.Hash
+	newBlock.Hash = calculateHash(newBlock)
+
+	return newBlock, nil
+}
+
+func isBlockValid(newBlock, oldBlock Block) bool {
+	if oldBlock.Index+1 != newBlock.Index {
+		return false
+	}
+
+	if oldBlock.Hash != newBlock.PrevHash {
+		return false
+	}
+
+	if calculateHash(newBlock) != newBlock.Hash {
+		return false
+	}
+
+	return true
+}
+
+func replaceChain(newBlocks []Block) {
+	if len(newBlocks) > len(Blockchain) {
+		Blockchain = newBlocks
+	}
+}
+
+func main() {
+	t := time.Now()
+	genesisBlock := Block{0, t.String(), "Genesis Block", "", ""}
+	fmt.Println(genesisBlock)
+	Blockchain = append(Blockchain, genesisBlock)
 }
